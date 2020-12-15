283d5d15d2d989ae9d7f9800f8208a0b14a80978
Jaswanth-Vardireddy <71151981+Jaswanth-Vardireddy@users.noreply.github.com>
2020-12-15 17:03:01 -0600

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,32 @@
+class Form
+{
+constructor()
+{
+
+}
+ display()
+ {
+ var title=createElement("h2")
+ title.html("car racing game")
+ title.position(130,0)
+ var input=createInput("NAME")
+ input.position(130,160)
+ var button=createButton("Click Here")
+ button.position(250,200)
+
+ button.mousePressed(function(){
+ input.hide();
+ button.hide();
+ var name=input.value();
+ playerCount+=1
+ player.updateName(name)
+ player.updateCount(playerCount)
+ var greeting=createElement("h2")
+ greeting.html("hello "+name)
+ greeting.position(130,160)
+ })
+ }
+
+
+
+}
